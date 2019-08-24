e9168f350bef2022cc5bce25ae7e557c478ac749
James Miller <james.rex.miller.4@gmail.com>
2019-08-24 17:00:04 -0600

    Add exercises 18-20

---
+++ ex19-2.py
@@ -0,0 +1,10 @@
+def hours_left_to_work(time_at_turing, whatever_is_left):
+    print(f"I am at Turing for {time_at_turing} hours a day.")
+    print(f"Outside of Turing I am left with {whatever_is_left} hours.")
+    print(f"Removing time spent sleeping, I only have {whatever_is_left - 7} hours.")
+    print("Guess that means it's time to go to work!")
+
+# time_at_turing = 7
+# whatever_is_left = 17
+
+hours_left_to_work (7, 17)
