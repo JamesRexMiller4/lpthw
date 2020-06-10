f6a45706be39e5f7d3d3692b07077a04ba0e7704
James Miller <james.rex.miller.4@gmail.com>
2020-06-09 21:16:18 -0600

    Complete exercises 33 and 34

---
+++ ex34.py
@@ -0,0 +1,25 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# Animal at index 1 
+print(animals[1])
+
+# The third animal
+print(animals[2])
+
+# The first animal
+print(animals[0])
+
+# Animal at 3 
+print(animals[3])
+
+# Fifth animal
+print(animals[4])
+
+# Animal at 2
+print(animals[2])
+
+# Sixth animal
+print(animals[5])
+
+# Animal at four
+print(animals[4])
